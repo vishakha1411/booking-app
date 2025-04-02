41e13ff4ed11c183e9d2836ea19f1088eda98a5b
vishakha1411 <119448495+vishakha1411@users.noreply.github.com>
2025-04-03 03:48:02 +0530

    Update PlacePage.jsx

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import { differenceInCalendarDays } from "date-fns"
-import Images from 'C:\\Users\\Vishakha\\airbnb\\client\\src\\Images.jsx'
+import Images from '../Images.jsx'
 
 const PlacePage = () => {
   const { id } = useParams()
